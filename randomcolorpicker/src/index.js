aeed8600544519b48ec674d9c28f97cd7cd9901d
JustBarnt <funtnal@gmail.com>
2022-07-08 11:05:48 -0400

    Added profile page example, and random color picker

--- randomcolorpicker/src/index.js
+++ randomcolorpicker/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {ProfilePage} from './ProfilePage';
 import reportWebVitals from './reportWebVitals';
+
+import { Random } from './Random';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ProfilePage />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Random />
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
